c50372e49aaccdaf748d6deab8471a97cb0c2cb2
Dmitry <demkas080797@mail.ru>
2023-04-01 15:19:08 +0500

    ДЗ 2 семинара

---
+++ zadacha7/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+Console.Clear();
+Console.Write("Ввести день невдели: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+if(numberDayOfWeek < 1 || numberDayOfWeek > 7)
+{
+    Console.WriteLine("Введен неправельный номер: ");
+    return;
+}
+if(numberDayOfWeek < 5)
+{
+    Console.WriteLine("Будни");
+}
+else if(numberDayOfWeek > 5)
+{
+Console.WriteLine("Выходной");
+}
