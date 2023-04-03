9b246fdc25cacf9aed9d498e20e3729bf4e930e4
Dmitry <demkas080797@mail.ru>
2023-04-03 14:08:23 +0500

    Решение ДЗ 3

---
+++ zadacha8/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int pal = 0, num = number;
+while (number > 0) {pal = pal * 10 + number % 10; number /= 10;}
+if(num == pal){
+    Console.WriteLine("палиндром");
+}else{
+    Console.WriteLine("НЕ палиндром");
+}
+
+ 
