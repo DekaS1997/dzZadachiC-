bfcb59715b2dffb2333bacbd90544088d45f4d9e
Dmitry <demkas080797@mail.ru>
2023-04-08 11:56:27 +0500

    ДЗ4

---
+++ zadacha12/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+int SumDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр в числе {number} = {SumDigit(number)}");
